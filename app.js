a0d339c31d8f8369ad5de4098f27c02ec5899e35
Adam Lord <adam@tivix.com>
2021-07-28 13:01:28 -0700

    use db file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 3001;
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('planetdb.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
